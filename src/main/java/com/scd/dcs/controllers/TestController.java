42607726f7b94859da7bcd7bda51b22bcb4e8702
lsg9134 <147814751+lsg9134@users.noreply.github.com>
2024-05-29 18:44:12 +0900

    2024.05.29 LSG

--- src/main/java/com/scd/dcs/controllers/TestController.java
+++ src/main/java/com/scd/dcs/controllers/TestController.java
@@ -2,6 +2,7 @@ package com.scd.dcs.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
