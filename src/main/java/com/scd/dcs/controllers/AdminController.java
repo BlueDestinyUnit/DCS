5de6d3f9789c932607d9c5ee9766516e47a10305
lsg9134 <147814751+lsg9134@users.noreply.github.com>
2024-05-30 18:38:24 +0900

    2024.05.30 LSG

--- src/main/java/com/scd/dcs/controllers/AdminController.java
+++ src/main/java/com/scd/dcs/controllers/AdminController.java
@@ -4,11 +4,14 @@ import com.scd.dcs.domains.entities.AttendanceEntity;
 import com.scd.dcs.services.AdminService;
 import com.scd.dcs.services.SalaryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("Admin")
+@RequestMapping
 public class AdminController {
     private final AdminService adminService;
     private final SalaryService salaryService;
@@ -17,5 +20,15 @@ public class AdminController {
     public AdminController(AdminService adminService, SalaryService salaryService) {
         this.adminService = adminService;
         this.salaryService = salaryService;
+    }
+
+    @RequestMapping(value = "/admin", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView getAdmin() {
+        return new ModelAndView("admin");
+    }
+
+    @RequestMapping(value = "/admin2", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView getAdmin2() {
+        return new ModelAndView("admin2");
     }
 }
