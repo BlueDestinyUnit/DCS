15d7a0d805c2735e1a7c62bfd72bd9a3c77185f3
BlueDestinyUnit <143476178+BlueDestinyUnit@users.noreply.github.com>
2024-06-14 18:43:57 +0900

    24.06.14 JSH 게시판 수정

--- src/main/java/com/scd/dcs/domains/vos/BoardVo.java
+++ src/main/java/com/scd/dcs/domains/vos/BoardVo.java
@@ -14,7 +14,7 @@ public class BoardVo extends BoardEntity {
    private String keyword;
 
    private int requestPage = 1; // 요청한 페이지 번호
-   private int countPerPage  = 10; // 한 페이지당 표시할 게시글(줄)의 개수
+   private int countPerPage  = 5; // 한 페이지당 표시할 게시글(줄)의 개수
    private int totalCount;
    private int minPage = 1;
    private int maxPage;
